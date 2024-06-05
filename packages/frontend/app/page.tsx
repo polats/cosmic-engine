55baf8acc1b15f36cacc0fd7c2c145232ec3fe20
jaeson-outplay <jaeson@outplay.games>
2024-06-06 02:23:02 +0800

    adding bg as background and added styling

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -7,8 +7,23 @@ export default function Home() {
       <Head>
         <title>Farcaster AuthKit + NextAuth Demo</title>
       </Head>
-      <main className="flex flex-col grow" style={{ fontFamily: "Inter, sans-serif"}}>
-        <JackpotJunction />
+      <main 
+        className="flex flex-col grow relative" 
+        style={{ 
+          fontFamily: "Inter, sans-serif",
+          background: "linear-gradient(0deg, #C30B9C 0%, #569FBF 63%, #95C7DD 100%)"
+        }}
+      >
+        <div className="absolute inset-0" style={{
+          backgroundImage: `url(/line-bg.png)`,
+          backgroundRepeat: "repeat",
+          backgroundSize: "auto",
+          backgroundPosition: "center",
+          opacity: 0.1}}
+        ></div>
+        <div className="relative z-[10] flex flex-col grow ">
+          <JackpotJunction />
+        </div>
       </main>
     </>
   );
