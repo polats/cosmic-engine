d23fd13f2deab97b788ff4b843f115b5cf5a8b3b
Paul Gadi <paulgadi@gmail.com>
2024-06-15 11:38:17 -0400

    remove placeholder UI

--- packages/frontend/components/cosmic-engine/JackpotJunction.tsx
+++ packages/frontend/components/cosmic-engine/JackpotJunction.tsx
@@ -116,33 +116,7 @@ export const JackpotJunction = () => {
 
     return (
         <div className="page-container">
-            <div style={{ position: "fixed", top: "80px", right: "12px" }}>
-                <SignInButton
-                    nonce={getNonce}
-                    onSuccess={handleSuccess}
-                    onError={() => setError(true)}
-                    onSignOut={() => signOut()}
-                />
-                {error && <div>Unable to sign in at this time.</div>}
-            </div>
-            <div style={{ position: "fixed", top: "140px", right: "12px" }}>
-                {
-                ready && authenticated ? 
-                    <button
-                        onClick={logout}
-                        className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
-                    >
-                        Logout
-                    </button>     
-                :
-                <button
-                    className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg"
-                    onClick={login}
-                >
-                    Privy Login
-                </button>                    
-                }
-            </div>   
+ 
             <div className="flex flex-col justify-center items-center grow text-center">
                 <GameAccountDisplay />
                 {/* TODO: Add array for prize pool to make wheel dynamic  */}
@@ -175,8 +149,39 @@ export const JackpotJunction = () => {
                     </button>                      
                 }
                 </div>
-                <Profile />
             </div>
         </div>
     );
 };
+
+/* auth UI
+
+            <div style={{ position: "fixed", top: "80px", right: "12px" }}>
+                <SignInButton
+                    nonce={getNonce}
+                    onSuccess={handleSuccess}
+                    onError={() => setError(true)}
+                    onSignOut={() => signOut()}
+                />
+                {error && <div>Unable to sign in at this time.</div>}
+            </div>
+            <div style={{ position: "fixed", top: "140px", right: "12px" }}>
+                {
+                ready && authenticated ? 
+                    <button
+                        onClick={logout}
+                        className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
+                    >
+                        Logout
+                    </button>     
+                :
+                <button
+                    className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg"
+                    onClick={login}
+                >
+                    Privy Login
+                </button>                    
+                }
+            </div>  
+            <Profile />
+*/
