722ccc54967899c497e4450de23d09d2a85ac7cc
Paul Gadi <paulgadi@gmail.com>
2024-06-23 18:52:12 -0400

    update item indices

--- packages/frontend/lib/constants.ts
+++ packages/frontend/lib/constants.ts
@@ -2,44 +2,44 @@ export const JJ_CONTRACT_NAME = "JackpotJunction";
 export const ROLL_COST = "100";
 export const ITEM_ID_IMAGE_LAYER_NAMES = [
     // 0
-    ["plains_cover", "Shady Adventurer"],
-    ["plains_body", "Scythe Body"],
-    ["plains_fwheel", "Grass Cutters"],
-    ["plains_beast", "Two-Headed Ox"],
+    ["plains_cover", "Shady Adventurer (Plains Cover)"],
+    ["plains_body", "Scythe Body (Plains Body)"],
+    ["plains_fwheel", "Grass Cutters (Plains Wheel)"],
+    ["plains_beast", "Two-Headed Ox (Plains Beast)"],
 
     // 1
-    ["forest_cover", "Mossy Camouflage"],
-    ["forest_body", "Chainsaw Body"],
-    ["forest_fwheel", "ATV Tires"],
-    ["forest_beast", "Giant Sloth"],
+    ["forest_cover", "Mossy Camouflage (Forest Cover)"],
+    ["forest_body", "Chainsaw Body (Forest Body)"],
+    ["forest_fwheel", "ATV Tires (Forest Wheel)"],
+    ["forest_beast", "Giant Sloth (Forest Beast)"],
 
     // 2
-    ["swamp_cover", "Mosquito Netting"],
-    ["swamp_body", "Fisherman Body"],
-    ["swamp_fwheel", "Mud Flaps"],
-    ["swamp_beast", "Alligator"],
+    ["swamp_cover", "Mosquito Netting (Swamp Cover)"],
+    ["swamp_body", "Fisherman Body (Swamp Body)"],
+    ["swamp_fwheel", "Mud Flaps (Swamp Wheel)"],
+    ["swamp_beast", "Alligator (Swamp Beast)"],
 
     // 3
-    ["water_cover", "Waterproof Tarp"],
-    ["water_body", "Inflatable Body"],
-    ["water_fwheel", "Displacement Discs"],
-    ["water_beast", "Giant Capybara"],
+    ["water_cover", "Waterproof Tarp (Water Cover)"],
+    ["water_body", "Inflatable Body (Water Body)"],
+    ["water_fwheel", "Displacement Discs (Water Wheel)"],
+    ["water_beast", "Giant Capybara (Water Beast)"],
 
     // 4
-    ["mountain_cover", "Rockslide Shield"],
-    ["mountain_body", "Dent-proof Body"],
-    ["mountain_fwheel", "Ratcheting Rollers"],
-    ["mountain_beast", "Mountain Goat"],
+    ["mountain_cover", "Rockslide Shield (Mountain Cover)"],
+    ["mountain_body", "Dent-proof Body (Mountain Body)"],
+    ["mountain_fwheel", "Ratcheting Rollers (Mountain Wheel)"],
+    ["mountain_beast", "Mountain Goat (Mountain Beast)"],
 
     // 5
-    ["desert_cover", "Sun Canopy"],
-    ["desert_body", "Ventilated Body"],
-    ["desert_fwheel", "Tank Treads"],
-    ["desert_beast", "Camel-sized Camel Spider"],
+    ["desert_cover", "Sun Canopy (Desert Cover)"],
+    ["desert_body", "Ventilated Body (Desert Body)"],
+    ["desert_fwheel", "Tank Treads (Desert Wheel)"],
+    ["desert_beast", "Camel-sized Camel Spider (Desert Beast)"],
 
     // 6
-    ["ice_cover", "Fur-Lined Cover"],
-    ["ice_body", "Insulated Body"],
-    ["ice_fwheel", "Spiked Wheels"],
-    ["ice_beast", "Giant Walrus"]
+    ["ice_cover", "Fur-Lined Cover (Ice Cover)"],
+    ["ice_body", "Insulated Body (Ice Body)"],
+    ["ice_fwheel", "Spiked Wheels (Ice Wheel)"],
+    ["ice_beast", "Giant Walrus (Ice Beast)"],
 ]
